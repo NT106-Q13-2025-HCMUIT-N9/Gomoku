8749cdbd801501605c6bb10755630d40d005b2ae
Mothmon14682 <huynhanhtuan14686@gmail.com>
2025-12-02 17:19:11 +0700

    Thêm tính năng cho server, đã có thể đồng bộ đồng hồ qua socket

---
+++ Gomoku_Server/MatchHandle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku_Server
+{
+    enum PlayerTurn
+    {
+        player1,
+        player2
+    }
+
+    internal class MatchHandle
+    {
+        PlayerTurn current_turn = PlayerTurn.player1;
+        TcpClient player1;
+        TcpClient player2;
+        int clock1;
+        int clock2;
+
+        public void StartClock()
+        {
+            try
+            {
+                while (ServerUtils.StillConnected(player1.Client) && ServerUtils.StillConnected(player2.Client))
+                {
+                    if (current_turn == PlayerTurn.player1)
+                    {
+                        string message = $"[TIME1];{clock1}";
+                        ServerUtils.SendMessage(player1.Client, message);
+                        ServerUtils.SendMessage(player2.Client, message);
+                        clock1 -= 1;
+                    }
+                    else if (current_turn == PlayerTurn.player2)
+                    {
+                        string message = $"[TIME2];{clock2}";
+                        ServerUtils.SendMessage(player1.Client, message);
+                        ServerUtils.SendMessage(player2.Client, message);
+                        clock1 -= 1;
+                    }
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("[LOG]: A match end");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR]: " + ex.ToString());
+            }
+        }
+
+        public void SwitchPlayer()
+        {
+            if(current_turn == PlayerTurn.player1)
+            {
+                current_turn = PlayerTurn.player2;
+            }
+            else
+            {
+                current_turn = PlayerTurn.player1;
+            }
+        }
+
+        public MatchHandle(TcpClient player1, TcpClient player2, int clock1, int clock2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+            this.clock1 = clock1;
+            this.clock2 = clock2;
+        }
+    }
+}
--- Gomoku_Server/Program.cs
+++ Gomoku_Server/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Gomoku_Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Server
+    {
+        private void StartMatch(TcpClient player1, TcpClient player2)
+        {
+            int clock1 = 600;
+            int clock2 = 600;
+
+            MatchHandle matchHandle = new MatchHandle(player1, player2, clock1, clock2);
+
+            try
+            {
+                ServerUtils.SendMessage(player1.Client, $"[INNIT];{clock1};{clock2};X");
+                ServerUtils.SendMessage(player2.Client, $"[INNIT];{clock1};{clock2};O");
+
+                Thread clock_thread = new Thread(() => matchHandle.StartClock());
+                clock_thread.Start();
+                clock_thread.Join();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR]: " + ex.ToString());
+            }
+        }
+
+        public Server()
+        {
+            Queue<TcpClient> waiting_queue = new Queue<TcpClient>();
+
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
+            listener.Start();
+
+            Thread listen_thread = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+
+                        lock (waiting_queue)
+                        {
+                            waiting_queue.Enqueue(client);
+
+                            Console.WriteLine($"[LOG]: Connected to {client.Client.RemoteEndPoint?.ToString()}");
+
+                            if (waiting_queue.Count >= 2)
+                            {
+                                TcpClient player1 = waiting_queue.Dequeue();
+                                TcpClient player2 = waiting_queue.Dequeue();
+
+                                if (!ServerUtils.StillConnected(player1.Client))
+                                {
+                                    waiting_queue.Enqueue(player2);
+                                    continue;
+                                }else if (!ServerUtils.StillConnected(player2.Client))
+                                {
+                                    waiting_queue.Enqueue(player1);
+                                    continue;
+                                }
+
+                                StartMatch(player1, player2);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[ERROR]: " + ex.ToString());
+                }
+            });
+            listen_thread.Start();
+
+            Console.WriteLine("[LOG]: Server is ready to listen for client");
+            listen_thread.Join();
+        }
+    }
+
+    internal class MainClass()
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+        }
+    }
+}

5ed1a50dd7c15e5ccc10843da0fe03d3a4280378
zr0x8 <zr0x8j@gmail.com>
2025-12-13 19:50:07 +0700

    added settings menu

---
+++ Gomoku_Client/Converters/SliderValueToWidthConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Gomoku_Client.Converters
+{
+  public class SliderValueToWidthConverter : IMultiValueConverter
+  {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (values.Length == 4 &&
+          values[0] is double currentValue &&
+          values[1] is double minimum &&
+          values[2] is double maximum &&
+          values[3] is double actualWidth)
+      {
+        if (maximum - minimum == 0)
+          return 0.0;
+
+        double percentage = (currentValue - minimum) / (maximum - minimum);
+
+        return actualWidth * percentage;
+      }
+
+      return 0.0;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

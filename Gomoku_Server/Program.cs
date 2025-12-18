3047d2178149d0251f320eca23df53c291cd6f41
Hoang Tuan <hdatuaans496@gmail.com>
2025-12-18 16:16:02 +0700

    Server Handle cơ bản chức năng thách đấu

--- Gomoku_Server/Program.cs
+++ Gomoku_Server/Program.cs
@@ -1,12 +1,18 @@
 ﻿using Gomoku_Server;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Permissions;
 using System.Text;
 
 namespace Server
 {
     class Server
     {
+
+        ConcurrentDictionary<string, TcpClient> challenges = new ConcurrentDictionary<string, TcpClient>();
+
+
         private void StartMatch(TcpClient player1, TcpClient player2)
         {
             int clock1 = 600;
@@ -16,20 +22,23 @@ namespace Server
 
             try
             {
-                ServerUtils.SendMessage(player1.Client, $"[INNIT];{clock1};{clock2};X");
-                ServerUtils.SendMessage(player2.Client, $"[INNIT];{clock1};{clock2};O");
+                ServerUtils.SendMessage(player1.Client, $"[INIT];{clock1};{clock2};X");
+                ServerUtils.SendMessage(player2.Client, $"[INIT];{clock1};{clock2};O");
 
-                Thread clock_thread = new Thread(() => {
+                Thread clock_thread = new Thread(() =>
+                {
                     matchHandle.StartClock();
                 });
                 clock_thread.Start();
 
-                Thread player1_thread = new Thread(() => {
+                Thread player1_thread = new Thread(() =>
+                {
                     matchHandle.Handle_Player1();
                 });
                 player1_thread.Start();
 
-                Thread player2_thread = new Thread(() => {
+                Thread player2_thread = new Thread(() =>
+                {
                     matchHandle.Handle_Player2();
                 });
                 player2_thread.Start();
@@ -44,6 +53,8 @@ namespace Server
             }
         }
 
+
+
         public Server()
         {
             Queue<TcpClient> waiting_queue = new Queue<TcpClient>();
@@ -51,6 +62,11 @@ namespace Server
             TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 9999);
             listener.Start();
 
+            new Thread(() =>
+            {
+                StartLobbyServer();
+            }).Start();
+        
             Thread listen_thread = new Thread(() =>
             {
                 try
@@ -62,7 +78,7 @@ namespace Server
                         lock (waiting_queue)
                         {
                             waiting_queue.Enqueue(client);
-
+                            
                             Console.WriteLine($"[LOG]: Connected to {client.Client.RemoteEndPoint?.ToString()}");
 
                             if (waiting_queue.Count >= 2)
@@ -74,13 +90,20 @@ namespace Server
                                 {
                                     waiting_queue.Enqueue(player2);
                                     continue;
-                                }else if (!ServerUtils.StillConnected(player2.Client))
+                                }
+                                else if (!ServerUtils.StillConnected(player2.Client))
                                 {
                                     waiting_queue.Enqueue(player1);
                                     continue;
                                 }
 
-                                StartMatch(player1, player2);
+                                if ( player1 != null  && player2 != null )
+                                {
+                                    new Thread(() =>
+                                    {
+                                        StartMatch(player1, player2);
+                                    }).Start();
+                                }
                             }
                         }
                     }
@@ -92,10 +115,94 @@ namespace Server
             });
             listen_thread.Start();
 
-            Console.WriteLine("[LOG]: Server is ready to listen for client");
+            Console.WriteLine("[LOG]: Server is running on 9999 (Match)");
             listen_thread.Join();
         }
+
+        public void StartLobbyServer()
+        {
+            TcpListener challengeListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8888);
+            challengeListener.Start();
+            Console.WriteLine("[LOG]: Server is running on 8888 (Lobby)");
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    TcpClient client = challengeListener.AcceptTcpClient();
+                    new Thread(() =>
+                    {
+                        HandleChallenge(client);
+                    }).Start();
+                }
+            }).Start();
+        }
+
+        public void HandleChallenge(TcpClient client)
+        {
+            string room = null;
+            var stream = client.GetStream();
+            byte[] data = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    int byteRead = stream.Read(data, 0, data.Length);
+                    if ( byteRead == 0) break;
+
+
+                    string message = Encoding.UTF8.GetString(data, 0, byteRead).Trim();
+                    string[] parts = message.Split('|');
+                    if (parts.Length < 3) continue;
+
+
+                    string command = parts[0];
+                    room = parts[1] + parts[2];
+
+
+                    Console.WriteLine(message);
+                    switch (command)
+                    {
+                        case "CHALLENGE_REQUEST":
+                            challenges.TryAdd(room, client);
+                            break;
+
+                        case "CHALLENGE_ACCEPT":
+                            if (challenges.TryRemove(room, out TcpClient challengeClient))
+                            {
+                                new Thread(() =>
+                                {
+                                    StartMatch(challengeClient, client);
+                                }).Start();
+
+                            }
+                            break;
+
+                        case "CHALLENGE_REJECT":
+                            challenges.TryRemove(room, out TcpClient removedClient);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (room != null)
+                    challenges.TryRemove(room, out _);
+
+                client.Close();
+            }
+        }
+
+
     }
+
 
     internal class MainClass()
     {

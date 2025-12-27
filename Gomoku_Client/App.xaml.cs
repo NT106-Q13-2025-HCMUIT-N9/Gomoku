72ae220dba34daa49dc25fe84ff5b08b6a8b6b95
zr0x8 <zr0x8j@gmail.com>
2025-12-27 13:07:09 +0700

    Allow only one instance

--- Gomoku_Client/App.xaml.cs
+++ Gomoku_Client/App.xaml.cs
@@ -12,10 +12,25 @@ namespace Gomoku_Client
 
     public partial class App : Application
     {
+        private const string AppUid = "5C3C3EA0-AB5E-47EC-AC79-BF6CCB1F895D";
+        private static Mutex? _mutex = null;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            FirebaseInfo.AppInit();
+            bool isNew;
+
+            _mutex = new Mutex(true, AppUid, out isNew);
+
+            if (isNew)
+            {
+                base.OnStartup(e);
+                FirebaseInfo.AppInit();
+            }
+            else
+            {
+                Shutdown();
+                return;
+            }
         }
     }
 }
